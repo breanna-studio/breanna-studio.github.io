0b220e620d34ec67e0c3d7a236dfbd7d86bd815a
breanna-studio <breannarob@gmail.com>
2023-01-06 23:16:49 -0600

    Add files via upload

--- luella.js
+++ luella.js
@@ -1,5 +1,18 @@
-setInterval(function() {
+var imgArray =[
+'images/aftm-1.jpeg',
+'images/AFTM-2.png',
+'images/AFTM-5.png'
+];
 
-	$('.holder div').css('left', '-80vw')
+var curIndex = 0;
 
-}, 7500)
+var imgDuration = 5000;
+
+function slideShow() {
+  document.getElementById('scope').src = imgArray[curIndex];
+  curIndex++;
+  if (curIndex == imgArray.length) { curIndex = 0; }
+    setTimeout("slideShow()", imgDuration);
+  }
+
+  slideShow();
